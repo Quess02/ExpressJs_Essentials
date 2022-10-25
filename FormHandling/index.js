d5fb9324d0e7744051eff328ef5f7091b9defcb5
Quess02 <65718377+Quess02@users.noreply.github.com>
2022-10-25 17:24:29 +0200

    Add files via upload

--- FormHandling/index.js
+++ FormHandling/index.js
@@ -1,1 +1,45 @@
+const express = require('express');
+const app = express();
+const path =require('path');
+const routes = require('./routes/formHandling');
 
+app.use(require('body-parser')());
+app.set('port', process.env.PORT || 3000);
+
+//set static file using express middleware
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', function (req, res) { 
+    res.sendFile(path.join(__dirname, 'public', 'form.html'));
+});
+
+//homepage route
+app.get('/home', (req, res)=>{ 
+    res.send(`Successful login `);
+});
+
+
+//processing forms
+app.use('/process', routes);
+
+//custom 404 page
+app.use((req,res) => { 
+    res.type('text/plain');
+    res.status(404);
+    res.send('404. Not Found');
+});
+
+    
+//custom 500 page
+app.use((err, eq, res, next) => {
+    console.error(err.stack);
+    res.type('text/plain');
+    res.status(500);
+    res.send('500. Internal Server Error')
+});
+
+
+app.listen(app.get('port'), (req,res) => {
+    console.log(`Listening on http://localhost:${app.get('port')}`)
+})
