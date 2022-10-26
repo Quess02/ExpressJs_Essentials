7fbae913c147b00edf21f695d3b9ad6a9fd66129
Quess02 <65718377+Quess02@users.noreply.github.com>
2022-10-26 11:51:13 +0200

    Add files via upload

--- routing/routes/subdomains.js
+++ routing/routes/subdomains.js
@@ -1,1 +1,19 @@
+const express = require('express');
+/**
+ * Express does not take subdomains into account by default.
+ * -Install vhost(npm install --save vhost)
+ * 
+ * */
 
+
+
+const admin = express.Router();
+admin.get('/', function (req, res) {
+    res.send('admin site')
+});
+
+admin.get('/users', function (req,res) { 
+    res.send('Users works');
+});
+
+module.exports = admin;
