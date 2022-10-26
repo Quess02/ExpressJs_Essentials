0116330bbea8fac8ab11e14f1c3f979664212b8d
Quess02 <65718377+Quess02@users.noreply.github.com>
2022-10-26 11:53:10 +0200

    Add files via upload

--- routing/routes/api/loans.js
+++ routing/routes/api/loans.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const loans = express.Router();
 
+loans.get('/', (req, res) => {
+    res.send('Loans works');
+});
+
+//Route parameters
+loans.get('/:id', (req, res) => {
+    let id = req.params.id;
+    res.send(`Loan id ${id}`);
+});
+module.exports = loans;
