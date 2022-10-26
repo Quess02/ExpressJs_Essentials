726945058c32eed008cb3c661d3f3a453abef0fb
Quess02 <65718377+Quess02@users.noreply.github.com>
2022-10-26 14:56:28 +0200

    Add files via upload

---
+++ members_CRUD_operations_API/Members.js
@@ -0,0 +1,21 @@
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'johndoe@gmail.com',
+        status:'active'
+    },
+    {
+        id: 2,
+        name: 'Isaac Saw',
+        email: 'isaac02@gmail.com',
+        status:'active'
+    },
+    {
+        id:3,
+        name: 'Luke Morien',
+        email: 'lm122@gmail.com',
+        status:'active'
+    }
+];
+module.exports = members;
--- members_CRUD_operations_API/index.js
+++ members_CRUD_operations_API/index.js
@@ -1,1 +1,24 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 5000;
 
+const logger=require('./middleware/logger')
+
+//Init middleware
+//app.use(logger);
+//Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+//set static file using express middleware
+app.use(express.static(path.join(__dirname, 'public')));
+/**app.get('/', function (req, res) { 
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+ });*/
+
+//IMPLEMENTING A SIMPLE API
+//Members API Routes
+app.use('/api/members',require('./routes/api/members'))
+
+
+app.listen(PORT,()=>console.log(`Listening on port ${PORT} `));
