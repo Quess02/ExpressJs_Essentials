72e37936cec29ed781784614f1378d1b0b18c3ae
Quess02 <65718377+Quess02@users.noreply.github.com>
2022-10-29 15:48:34 +0200

    Add files via upload

--- User_CRUD_MongoDB/index.js
+++ User_CRUD_MongoDB/index.js
@@ -1,1 +1,20 @@
+const express=require('express');
+const server=express();
+const path=require('path');
+const PORT=process.env.PORT||3000;
 
+//users route middleware
+server.use('/user',require('./routes/user'));
+//static file middleware
+server.use(express.static(path.join(__dirname,'public')));
+
+//entry route
+server.get('/',(req,res)=>{
+    res.type('html');
+    res.sendFile(path.join(__dirname,'public','/templates/home.html'));
+});
+
+
+server.listen(PORT,()=>{
+    console.log('Server started on http://localhost:'+PORT);
+})
